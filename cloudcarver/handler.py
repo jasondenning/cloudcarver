bba277549de05f7f17788a296b22bef66795f9b8
Jason Denning <jason@ngeniux.com>
2014-05-03 00:36:54 -0700

    Cleanup physical resource id generation

--- cloudcarver/handler.py
+++ cloudcarver/handler.py
@@ -1,5 +1,7 @@
 __author__ = 'Jason Denning'
 
+from cloudcarver.util import short_uuid
+
 import logging
 
 log = logging.getLogger(__name__)
@@ -19,14 +21,15 @@ class Handler(object):
     A physical resource id will be generated in the 'create' case, otherwise the
     physical_resource_id from the request will be used.
 
-    As with create, et. al.,  _set_physical_resource_id() may be defined as
-    _set_physical_resource_id or as _do_set_physical_resource_id.
+    As with create(), et. al.,  _generate_resource_id() may be defined as
+    _generate_physical_id or as _do_generate_physical_id.
     """
 
     def __init__(self, request):
         self._request = request
+        self._physical_resource_id = None
         self._request_type = request.request_type.lower()
-        self._set_physical_resource_id()
+
 
     @property
     def request(self):
@@ -40,17 +43,50 @@ class Handler(object):
     def request_type(self):
         return self._request_type
 
-    def _set_physical_resource_id(self):
+
+    @property
+    def physical_resource_id(self):
+        if not self._physical_resource_id:
+            self._physical_resource_id = self._get_or_generate_physical_id()
+        return self._physical_resource_id
+
+    def _get_or_generate_physical_id(self):
         """
         Either generates a new, unique(ish) physical resource id (in the case
         of a create request), or sets self.physical_resource_id (really
         self._physical_resource_id) to whatever is specified in the request.
         """
         if self.request_type == 'create':
-            log.debug("Got 'create' request - generating a new pysical_resource_id")
-            if hasattr(self, '_do_set_physical_resource_id'):
-                self._do_set_physical_resource_id()
-                log.info("Generated physical_resource_id: %s " %
-                    self.physical_resource_id)
-            else:
-                physical_resource_id = self.__name__ + ''
+            log.debug("Got create request - generating a new physical_resource_id")
+            physical_id = self._generate_physical_id()
+            log.info("Got generated physical resource id %s" % physical_id)
+        else:
+            log.debug("Got %s request - using physical resource id from request" %
+                self._request_type)
+            physical_resource_id = self.request['PhysicalResourceId']
+            log.info("Got physical resource id %s from request")
+
+        return physical_resource_id
+
+
+    def _generate_physical_id(self):
+        """
+        Generates a unique "physical" resource id.  The format of the name may
+        be adjusted in the application config. <- FIXME
+
+        If self._do_generate_physical_id() is defined, that method will be used
+        instead.
+        :param base_name:
+        :return: (String) A semi-unique resource_id
+        """
+        log.info("Generating physical resource id")
+        if hasattr(self, '_do_generate_physical_id'):
+            log.debug("Using _do_generate_physical_id method")
+            physical_id = self._do_generate_physical_id()
+        else:
+            base_name = self.__name__
+            physical_id = base_name + short_uuid()
+
+        log.debug("Successfuly generated physical resource id: %s " % physical_id)
+        return physical_id
+
